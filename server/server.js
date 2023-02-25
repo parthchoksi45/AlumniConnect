7fcc4dfc03835f95b740de2672b09c119b460822
Parth Choksi <parthchoksi45@gmail.com>
2023-02-25 17:38:10 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,13 @@ connectDB();
 
 //init Middleware
 app.use(express.json({extended: false}));
-
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    });
 
 const PORT = process.env.PORT||5000;
 
